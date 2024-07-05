0a594a9daebd46f981f002abd7c599a33acab8a6
shuiki <13360586528@163.com>
2024-07-05 21:31:39 +0800

    gene_contraAda

--- encoder/models/general_cf/lightgcn_gene_contraada.py
+++ encoder/models/general_cf/lightgcn_gene_contraada.py
@@ -1,19 +1,19 @@
 import pickle
 import torch as t
 from torch import nn
+import torch.nn.functional as F
 from config.configurator import configs
-from models.loss_utils import cal_bpr_loss, reg_params, cal_infonce_loss
+from models.aug_utils import NodeMask
 from models.base_model import BaseModel
 from models.model_utils import SpAdjEdgeDrop
+from models.loss_utils import cal_bpr_loss, reg_params, ssl_con_loss,cal_infonce_loss
 
 init = nn.init.xavier_uniform_
 uniformInit = nn.init.uniform
 
-# lightGCN(contrastive alignment) + contrastive adapter
-
-class LightGCN_plus_contraAda(BaseModel):
+class LightGCN_gene_contraAda(BaseModel):
     def __init__(self, data_handler):
-        super(LightGCN_plus_contraAda, self).__init__(data_handler)
+        super(LightGCN_gene_contraAda, self).__init__(data_handler)
         self.adj = data_handler.torch_adj
         self.keep_rate = configs['model']['keep_rate']
         self.user_embeds = nn.Parameter(init(t.empty(self.user_num, self.embedding_size)))
@@ -25,8 +25,9 @@ class LightGCN_plus_contraAda(BaseModel):
         # hyper-parameter
         self.layer_num = self.hyper_config['layer_num']
         self.reg_weight = self.hyper_config['reg_weight']
-        self.kd_weight = self.hyper_config['kd_weight']
-        self.kd_temperature = self.hyper_config['kd_temperature']
+        self.mask_ratio = self.hyper_config['mask_ratio']
+        self.recon_weight = self.hyper_config['recon_weight']
+        self.re_temperature = self.hyper_config['re_temperature']
 
         # semantic-embeddings
         self.usrprf_embeds = nn.Parameter(t.tensor(configs['usrprf_embeds']).float())
@@ -40,10 +41,14 @@ class LightGCN_plus_contraAda(BaseModel):
         self.usr_pos_sample_idx = t.tensor(configs['usr_pos_samples_idx']).T[1].long()
         self.itm_pos_sample_idx = t.tensor(configs['itm_pos_samples_idx']).T[1].long()
 
+        # self.prf_embeds = t.concat([usrprf_embeds, itmprf_embeds], dim=0)
+
+        # generative process
+        self.masker = NodeMask(self.mask_ratio, self.embedding_size)
         self.mlp = nn.Sequential(
-            nn.Linear(self.usrprf_embeds.shape[1], (self.usrprf_embeds.shape[1] + self.embedding_size) // 2),
+            nn.Linear(self.embedding_size, (self.usrprf_embeds.shape[1] + self.embedding_size) // 2),
             nn.LeakyReLU(),
-            nn.Linear((self.usrprf_embeds.shape[1] + self.embedding_size) // 2, self.embedding_size)
+            nn.Linear((self.usrprf_embeds.shape[1] + self.embedding_size) // 2, self.usrprf_embeds.shape[1])
         )
 
         self._init_weight()
@@ -52,16 +57,24 @@ class LightGCN_plus_contraAda(BaseModel):
         for m in self.mlp:
             if isinstance(m, nn.Linear):
                 init(m.weight)
-
+    
     def _propagate(self, adj, embeds):
         return t.spmm(adj, embeds)
+    
+    def _mask(self):
+        embeds = t.concat([self.user_embeds, self.item_embeds], axis=0)
+        masked_embeds, seeds = self.masker(embeds)
+        return masked_embeds[:self.user_num], masked_embeds[self.user_num:], seeds
 
-    def forward(self, adj=None, keep_rate=1.0):
+    def forward(self, adj=None, keep_rate=1.0, masked_user_embeds=None, masked_item_embeds=None):
         if adj is None:
             adj = self.adj
         if not self.is_training and self.final_embeds is not None:
             return self.final_embeds[:self.user_num], self.final_embeds[self.user_num:]
-        embeds = t.concat([self.user_embeds, self.item_embeds], axis=0)
+        if masked_user_embeds is None or masked_item_embeds is None:
+            embeds = t.concat([self.user_embeds, self.item_embeds], axis=0)
+        else:
+            embeds = t.concat([masked_user_embeds, masked_item_embeds], axis=0)
         embeds_list = [embeds]
         if self.is_training:
             adj = self.edge_dropper(adj, keep_rate)
@@ -79,18 +92,21 @@ class LightGCN_plus_contraAda(BaseModel):
         neg_embeds = item_embeds[negs]
         return anc_embeds, pos_embeds, neg_embeds
 
+    def _reconstruction(self, embeds, seeds):
+        enc_embeds = embeds[seeds]
+        prf_embeds = t.concat([self.usrprf_embeds, self.itmprf_embeds], dim=0)[seeds]
+        # prf_embeds = self.prf_embeds[seeds]
+        enc_embeds = self.mlp(enc_embeds)
+        recon_loss = ssl_con_loss(enc_embeds, prf_embeds, self.re_temperature)
+        return recon_loss
+
     def cal_loss(self, batch_data):
         self.is_training = True
-        user_embeds, item_embeds = self.forward(self.adj, self.keep_rate)
-
-        anc_embeds, pos_embeds, neg_embeds = self._pick_embeds(user_embeds, item_embeds, batch_data)
 
         usrprf_embeds = self.usrprf_embeds
         itmprf_embeds = self.itmprf_embeds
         # userprf_embeds_ori, posItemprf_embeds_ori, negItemprf_embeds_ori = self._pick_embeds(usrprf_embeds, itmprf_embeds, batch_data)
 
-        usrprf_embeds = self.mlp(usrprf_embeds)
-        itmprf_embeds = self.mlp(itmprf_embeds)
         ancprf_embeds, posprf_embeds, negprf_embeds = self._pick_embeds(usrprf_embeds, itmprf_embeds, batch_data)
 
         usr_pos_samples = usrprf_embeds[self.usr_pos_sample_idx]
@@ -98,21 +114,24 @@ class LightGCN_plus_contraAda(BaseModel):
         usr_match_samples, itm_pos_match_samples, itm_neg_match_samples = self._pick_embeds(usr_pos_samples,
                                                                                             itm_pos_samples, batch_data)
         uu_loss = self.w_uu * cal_infonce_loss(ancprf_embeds, usr_match_samples, usrprf_embeds, self.kd_temperature)
-        ii_loss = self.w_ii * (cal_infonce_loss(posprf_embeds, itm_pos_match_samples, itmprf_embeds, self.kd_temperature) + \
-                               cal_infonce_loss(negprf_embeds, itm_neg_match_samples, itmprf_embeds, self.kd_temperature)) / 2
+        ii_loss = self.w_ii * (
+                    cal_infonce_loss(posprf_embeds, itm_pos_match_samples, itmprf_embeds, self.kd_temperature) + \
+                    cal_infonce_loss(negprf_embeds, itm_neg_match_samples, itmprf_embeds, self.kd_temperature)) / 2
 
+        masked_user_embeds, masked_item_embeds, seeds = self._mask()
 
+        user_embeds, item_embeds = self.forward(self.adj, self.keep_rate, masked_user_embeds, masked_item_embeds)
+        ancs, poss, negs = batch_data
+        anc_embeds = user_embeds[ancs]
+        pos_embeds = item_embeds[poss]
+        neg_embeds = item_embeds[negs]
         bpr_loss = cal_bpr_loss(anc_embeds, pos_embeds, neg_embeds) / anc_embeds.shape[0]
         reg_loss = self.reg_weight * reg_params(self)
 
-        kd_loss = cal_infonce_loss(anc_embeds, ancprf_embeds, usrprf_embeds, self.kd_temperature) + \
-                  cal_infonce_loss(pos_embeds, posprf_embeds, posprf_embeds, self.kd_temperature) + \
-                  cal_infonce_loss(neg_embeds, negprf_embeds, negprf_embeds, self.kd_temperature)
-        kd_loss /= anc_embeds.shape[0]
-        kd_loss *= self.kd_weight
+        recon_loss = self.recon_weight * self._reconstruction(t.concat([user_embeds, item_embeds], axis=0), seeds)
 
-        loss = bpr_loss + reg_loss + kd_loss + uu_loss + ii_loss
-        losses = {'bpr_loss': bpr_loss, 'reg_loss': reg_loss, 'kd_loss': kd_loss,'uu_loss': uu_loss,'ii_loss': ii_loss}
+        loss = bpr_loss + reg_loss + recon_loss + uu_loss + ii_loss
+        losses = {'bpr_loss': bpr_loss, 'reg_loss': reg_loss, 'recon_loss': recon_loss,'uu_loss': uu_loss,'ii_loss': ii_loss}
         return loss, losses
 
     def full_predict(self, batch_data):
